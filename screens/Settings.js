df2c8ee63418ce11568188c1e6b157c2cf39d78a
Nattawut Un <baszaa1234@gmail.com>
2023-10-11 19:50:59 +0700

    Update settings page header

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,5 +1,5 @@
 import { StyleSheet, View } from 'react-native'
-import { ActivityIndicator, Text } from 'react-native-paper'
+import { ActivityIndicator, Text, Appbar, MD3Colors } from 'react-native-paper'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Ionicons } from '@expo/vector-icons'
 
@@ -9,11 +9,11 @@ const Stack = createNativeStackNavigator()
 
 export default function Settings() {
   return (
-    <Stack.Navigator initialRouteName='SettingsHome'>
+    <Stack.Navigator initialRouteName='SettingsHome' screenOptions={{
+      header: SettingsHeader
+    }}>
       <Stack.Screen name='SettingsHome' component={SettingsHome} options={{
         title: 'Settings',
-        headerLeft: () => <Ionicons name="settings" size={24} color="black" style={{ marginRight: 12 }} />,
-        headerShown: false
       }} />
       <Stack.Screen name='SettingsA' component={Placeholder} options={{
         title: 'First Page',
@@ -29,6 +29,23 @@ function Placeholder() {
       <View style={{ marginVertical: 4 }} />
       <Text variant="bodyMedium">WIP</Text>
     </View>
+  )
+}
+
+function SettingsHeader({ navigation }) {
+  return (
+    <Appbar.Header
+      mode="small"
+      style={{
+        backgroundColor: MD3Colors.primary50,
+      }}
+    >
+      <Appbar.BackAction
+        color={MD3Colors.primary90}
+        onPress={() => navigation.goBack()}
+      />
+      <Appbar.Content title="Settings" color="white" />
+    </Appbar.Header>
   )
 }
 
--- screens/settings/SettingsHome.js
+++ screens/settings/SettingsHome.js
@@ -3,43 +3,32 @@ import { List, MD3Colors, Appbar } from 'react-native-paper'
 
 export default function SettingsHome({ navigation } ) {
   return (
-    <View>
-      <Appbar.Header
-          mode="small"
-          style={{
-            backgroundColor: MD3Colors.primary50,
-          }}
-        >
-          <Appbar.BackAction color={MD3Colors.primary90} onPress={() => navigation.goBack()} />
-        <Appbar.Content title='Settings' color='white' />
-      </Appbar.Header>
-      <ScrollView>
-        <List.Section>
-          <List.Subheader>Some title</List.Subheader>
-          <List.Item
-            title="First Item"
-            left={() => <List.Icon color={MD3Colors.primary50} icon="folder" />}
-            onPress={e => navigation.navigate('SettingsA')}
-            style={styles.listItem}
-          />
-          <List.Item
-            title="Second Item"
-            left={() => <List.Icon color={MD3Colors.primary50} icon="calendar" />}
-            onPress={e => navigation.navigate('SettingsA')}
-            style={styles.listItem}
-          />
-        </List.Section>
-        <List.Section>
-          <List.Subheader>About</List.Subheader>
-          <List.Item
-            title="Version 0.0.1"
-            left={() => <List.Icon color='gray' icon="application-brackets-outline" />}
-            onPress={e => console.log('Version 0.0.1')}
-            style={styles.listItem}
-          />
-        </List.Section>
-      </ScrollView>
-    </View>
+    <ScrollView>
+      <List.Section>
+        <List.Subheader>Some title</List.Subheader>
+        <List.Item
+          title="First Item"
+          left={() => <List.Icon color={MD3Colors.primary50} icon="folder" />}
+          onPress={e => navigation.navigate('SettingsA')}
+          style={styles.listItem}
+        />
+        <List.Item
+          title="Second Item"
+          left={() => <List.Icon color={MD3Colors.primary50} icon="calendar" />}
+          onPress={e => navigation.navigate('SettingsA')}
+          style={styles.listItem}
+        />
+      </List.Section>
+      <List.Section>
+        <List.Subheader>About</List.Subheader>
+        <List.Item
+          title="Version 0.0.1"
+          left={() => <List.Icon color='gray' icon="application-brackets-outline" />}
+          onPress={e => console.log('Version 0.0.1')}
+          style={styles.listItem}
+        />
+      </List.Section>
+    </ScrollView>
   )
 }
 
