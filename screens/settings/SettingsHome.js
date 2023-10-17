101b0bb2cfcee3962267f71e065676677496e418
Nattawut Un <baszaa1234@gmail.com>
2023-10-17 19:19:40 +0700

    Add logout in settings

--- screens/settings/SettingsHome.js
+++ screens/settings/SettingsHome.js
@@ -1,34 +1,85 @@
 import { View, ScrollView, Text, StyleSheet } from 'react-native'
-import { List, MD3Colors, Appbar } from 'react-native-paper'
+import { List, MD3Colors, Appbar, PaperProvider } from 'react-native-paper'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+
+import { saveUser } from 'config/redux'
+import { LogoutModal } from 'components/modal'
+import { FIREBASE_AUTH } from 'config/firebase'
 
 export default function SettingsHome({ navigation } ) {
+  const user = useSelector(state => state.user.user)
+  const [showLogoutModal, setShowLogoutModal] = useState(false)
+
+  const logOut = () => {
+    console.log('Logout')
+    navigation.navigate('Home')
+    FIREBASE_AUTH.signOut()
+  }
+
   return (
-    <ScrollView>
-      <List.Section>
-        <List.Subheader>Some title</List.Subheader>
-        <List.Item
-          title="First Item"
-          left={() => <List.Icon color={MD3Colors.primary50} icon="folder" />}
-          onPress={e => navigation.navigate('SettingsA')}
-          style={styles.listItem}
-        />
-        <List.Item
-          title="Second Item"
-          left={() => <List.Icon color={MD3Colors.primary50} icon="calendar" />}
-          onPress={e => navigation.navigate('SettingsA')}
-          style={styles.listItem}
-        />
-      </List.Section>
-      <List.Section>
-        <List.Subheader>About</List.Subheader>
-        <List.Item
-          title="Version 0.0.1"
-          left={() => <List.Icon color='gray' icon="application-brackets-outline" />}
-          onPress={e => console.log('Version 0.0.1')}
-          style={styles.listItem}
-        />
-      </List.Section>
-    </ScrollView>
+    <PaperProvider>
+      <ScrollView>
+        <List.Section>
+          <List.Subheader>Some title</List.Subheader>
+          <List.Item
+            title="First Item"
+            left={() => <List.Icon color={MD3Colors.primary50} icon="folder" />}
+            onPress={e => navigation.navigate('SettingsA')}
+            style={styles.listItem}
+          />
+          <List.Item
+            title="Second Item"
+            left={() => (
+              <List.Icon color={MD3Colors.primary50} icon="calendar" />
+            )}
+            onPress={e => navigation.navigate('SettingsA')}
+            style={styles.listItem}
+          />
+        </List.Section>
+        <List.Section>
+          <List.Subheader>User</List.Subheader>
+          <List.Item
+            title="Your email"
+            description={user ? user.email : ''}
+            left={() => <List.Icon color="gray" icon="email" />}
+            onPress={e => console.log(user)}
+            style={styles.listItem}
+          />
+          <List.Item
+            title="Your UID"
+            description={user ? user.uid : ''}
+            left={() => <List.Icon color="gray" icon="account" />}
+            onPress={e => console.log(user)}
+            style={styles.listItem}
+          />
+          <List.Item
+            title="Logout"
+            titleStyle={{ color: 'red' }}
+            left={() => <List.Icon color="red" icon="logout" />}
+            onPress={e => setShowLogoutModal(true)}
+            style={styles.listItem}
+            rippleColor={'hsl(0, 50%, 80%)'}
+          />
+        </List.Section>
+        <List.Section title="App">
+          <List.Item
+            title="Version"
+            description="0.0.1"
+            left={() => (
+              <List.Icon color="gray" icon="application-brackets-outline" />
+            )}
+            onPress={e => console.log('Version 0.0.1')}
+            style={styles.listItem}
+          />
+        </List.Section>
+      </ScrollView>
+      <LogoutModal
+        visible={showLogoutModal}
+        onCancel={() => setShowLogoutModal(false)}
+        onOK={logOut}
+      />
+    </PaperProvider>
   )
 }
 
