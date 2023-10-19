f5ef9e2ba61215ad9a82be757815325793ae494f
Nattawut Un <baszaa1234@gmail.com>
2023-10-19 14:45:27 +0700

    Add new calendar page

--- screens/Calendar.js
+++ screens/Calendar.js
@@ -1,116 +1,58 @@
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { useEffect, useState } from "react";
-import { Card } from 'react-native-paper';
+import { Appbar, Card, MD3Colors } from 'react-native-paper';
 import dayjs from "dayjs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CalendarPage from "./calendar/CalendarPage";
+import CalendarDetails from "./calendar/CalendarDetails";
 
 const timeToString = time => {
   const date = new Date(time)
   return date.toISOString().split('T')[0]
 }
 
+const Stack = createNativeStackNavigator()
+
 export default function Calendar() {
-  const [selected, setSelected] = useState('')
-  const [items, setItems] = useState({
-    '2023-08-22': [{ name: 'item 1 - any js object' }],
-    '2023-08-23': [{ name: 'item 2 - any js object', height: 80 }],
-    '2023-08-25': [
-      { name: 'item 3 - any js object' },
-      { name: 'any js object' },
-    ],
-  })
-
-  const loadItems = day => {
-    // setTimeout(() => {
-    //   for (let i = -15; i < 85; i++) {
-    //     const time = day.timestamp + i * 24 * 60 * 60 * 1000
-    //     const strTime = timeToString(time)
-    //     if (!items[strTime]) {
-    //       items[strTime] = []
-    //       const numItems = Math.floor(Math.random() * 3 + 1)
-    //       for (let j = 0; j < numItems; j++) {
-    //         items[strTime].push({
-    //           name: 'Item for ' + strTime + ' #' + j,
-    //           height: Math.max(50, Math.floor(Math.random() * 150)),
-    //         })
-    //       }
-    //     }
-    //   }
-    //   const newItems = {}
-    //   Object.keys(items).forEach(key => {
-    //     newItems[key] = items[key]
-    //   })
-    //   setItems(newItems)
-    // }, 1000)
-    let currentDate = day.dateString
-    setSelected(currentDate)
-    let lowerDate = dayjs(currentDate).subtract(1, 'month').format('YYYY-MM-DD')
-    let upperDate = dayjs(currentDate).add(1, 'month').format('YYYY-MM-DD')
-    // console.log(lowerDate, currentDate, upperDate)
-
-    console.log(items)
-
-    // let newItems = items
-    // if (!newItems[currentDate]) {
-    //   newItems[currentDate] = []
-    // }
-    // setItems(newItems)
-  }
-
-  useEffect(() => {
-    let newItems = items
-    for (const [key, value] of Object.entries(newItems)) {
-      if (value.length <= 0) {
-        delete newItems[key]
-      }
-    }
-    if (!newItems[selected]) {
-      newItems[selected] = []
-    }
-    setItems(newItems)
-  }, [selected])
-
-  const renderItem = item => {
-    return (
-      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
-        <Card>
-          <Card.Content>
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}
-            >
-              <Text>{item.name}</Text>
-              {/* <Avatar.Text label="J" /> */}
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-    )
-  }
-
   return (
-    <View style={{ flex: 1 }}>
-      {/* <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={dayjs().format('YYYY-MM-DD')}
-        renderItem={renderItem}
-      /> */}
-    </View>
+    <Stack.Navigator initialRouteName="CalendarPage">
+      <Stack.Screen name="CalendarPage" component={CalendarPage} options={{ header: CalendarPageHeader }} />
+      <Stack.Screen name="CalendarDetails" component={CalendarDetails} options={{ header: CalendarDetailsHeader }} />
+    </Stack.Navigator>
   )
 }
 
-
-const noItem = () => {
+function CalendarPageHeader({ navigation }) {
   return (
-    <View style={{
-      backgroundColor: '#ccc',
-      padding: 8
-    }}>
-      <Text>no items...</Text>
-    </View>
+    <Appbar.Header style={{ backgroundColor: MD3Colors.primary50 }}>
+      <Appbar.Action icon="calendar" color={MD3Colors.primary90} />
+      <Appbar.Content title={dayjs().format('ddd, MMM DD')} color="white" />
+      <Appbar.Action
+        icon="calendar-today"
+        color={MD3Colors.primary90}
+        onPress={() => console.log('Today(?)')}
+      />
+    </Appbar.Header>
+  )
+}
+
+function CalendarDetailsHeader({ navigation, route }) {
+  const date = dayjs(route.params.date)
+
+  return (
+    <Appbar.Header style={{ backgroundColor: MD3Colors.primary50 }}>
+      <Appbar.BackAction
+        color={MD3Colors.primary90}
+        onPress={() => navigation.goBack()}
+      />
+      <Appbar.Content title={'Details'} color="white" />
+      <Appbar.Action
+        icon="dots-vertical"
+        color={MD3Colors.primary90}
+        onPress={() => console.log('More')}
+      />
+    </Appbar.Header>
   )
 }
 
---
+++ screens/calendar/CalendarPage.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, MD3Colors, Text } from "react-native-paper";
+import { useSelector } from "react-redux";
+import { CalendarProvider, CalendarList } from 'react-native-calendars'
+import { onSnapshot, Timestamp } from "firebase/firestore";
+import dayjs from "dayjs";
+import _ from "lodash";
+
+import { getAssignmentsCollection } from "services/firestore";
+import { parseAppointmentsToMarkedDates } from "services/calendar";
+
+export default function CalendarPage({ navigation, route }) {
+  const [currentDate, setCurrentDate] = useState(new Date())
+  const goToDate = date => navigation.navigate('CalendarDetails')
+
+  const user = useSelector(state => state.user.user)
+  const [assignments, setAssignments] = useState([])
+  const getAssignment = querySnapshot => {
+    const data = []
+    querySnapshot.forEach(res => {
+      const { title, description, dueDate } = res.data()
+      data.push({ key: res.id, title, description, dueDate })
+    })
+
+    const now = new Date()
+    const unfinishedData = data.filter(
+      item =>
+        now <
+        new Timestamp(item.dueDate.seconds, item.dueDate.nanoseconds).toDate()
+    )
+    const sortedData = _.sortBy(unfinishedData, item => item.dueDate.seconds)
+    setAssignments(sortedData)
+    console.log('Assignment fetched.')
+  }
+
+  const assignmentsQuery = getAssignmentsCollection(user ? user.uid : '')
+  useEffect(() => {
+    onSnapshot(assignmentsQuery, { next: getAssignment })
+  }, [])
+
+  const today = dayjs().format('YYYY-MM-DD')
+  const [groupedAssignments, markedAssignments] = parseAppointmentsToMarkedDates(assignments)
+  const markedDates = {
+    [today]: { selectedColor: MD3Colors.primary50, selected: true },
+    ...markedAssignments,
+  }
+
+  return (
+    <CalendarList
+      // (months) example: {"year":2024,"month":3,"day":18,"timestamp":1710720000000,"dateString":"2024-03-18"}
+      // onVisibleMonthsChange={months => console.log('CalendarPage: rendering ' + months.dateString)}
+      pastScrollRange={12}
+      futureScrollRange={12}
+      showScrollIndicator={true}
+      onDayPress={day =>
+        navigation.navigate('CalendarDetails', {
+          date: day.dateString,
+          assignments: groupedAssignments[day.dateString] ?? null,
+        })
+      }
+      current={dayjs().format('YYYY-MM-DD')}
+      renderHeader={renderCustomHeader}
+      markingType="custom"
+      markedDates={markedDates}
+    />
+  )
+}
+
+function renderCustomHeader(date) {
+  const header = date.toString('MMMM yyyy')
+  const [month, year] = header.split(' ')
+  const textStyle = {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingTop: 10,
+    paddingBottom: 10,
+    color: MD3Colors.primary50,
+    paddingRight: 5,
+  }
+
+  return (
+    <View style={styles.header}>
+      <Text style={[styles.month, textStyle]}>{`${month}`}</Text>
+      <Text style={[styles.year, textStyle]}>{year}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  month: {
+    marginLeft: 5
+  },
+  year: {
+    marginRight: 5
+  },
+  container: {
+    flexGrow: 1,
+    backgroundColor: '#fff',
+  },
+  fabStyle: {
+    bottom: 16,
+    right: 16,
+    position: 'absolute',
+  },
+})
