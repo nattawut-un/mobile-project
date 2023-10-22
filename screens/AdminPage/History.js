727fcf15c38233e8331f1c1395878b627721754f
Chayutarat <64070138@kmitl.ac.th>
2023-10-22 21:11:31 +0700

    Finish History
    finish mockup

--- screens/AdminPage/History.js
+++ screens/AdminPage/History.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, ScrollView, View } from 'react-native'
+import { Portal, PaperProvider, Text, Appbar, MD3Colors, Chip } from 'react-native-paper';
 import React from 'react'
+
+import ListCard from 'components/ListCard'
+import TestImage from 'assets/icon.png';
 
 const History = () => {
   return (
-    <View>
-      <Text>History</Text>
-    </View>
+    <ScrollView style={ styles.container }>
+      <View>
+        {[...Array(2).keys()].map((n, i) => (
+          <ListCard
+            key={i}
+            title={'Apple pencil'}
+            description={
+              "12:00:00, September 17,2023"
+            }
+            image={TestImage}
+          >
+            <Chip style={ styles.chip } onPress={() => console.log('Pressed')}>
+              Redeem
+            </Chip>
+          </ListCard>
+        ))}
+      </View>
+      {/* <View></View> */}
+    </ScrollView>
   )
 }
 
 export default History
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: 'white',
+  },
+  chip: {
+    marginTop: 10,
+    backgroundColor: "white",
+    width: 85
+  },
+})
